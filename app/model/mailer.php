b8ec3427b25209029d64709c0f6b68fb2115c344
Precious Tom <tomprezine@gmail.com>
2020-01-18 21:45:38 +0100

    off mail debug

--- app/model/mailer.php
+++ app/model/mailer.php
@@ -13,7 +13,7 @@ function sendEmail($recipientEmail, $recipientName, $subject, $html)
     $mail = new PHPMailer(true);
     try {
         //Server settings
-        $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+        $mail->SMTPDebug = 0;                                       // Enable verbose debug output
         $mail->isSMTP();                                            // Set mailer to use SMTP
         $mail->Host       = 'premium72.web-hosting.com';            // Specify main and backup SMTP servers
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
